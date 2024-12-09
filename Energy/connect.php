a8ecdec8ba9f68a078ae4f26a4163f2a7edf28ec
Levente <122294490+Levi-24@users.noreply.github.com>
2024-12-09 12:08:13 +0100

    connect

--- Energy/connect.php
+++ Energy/connect.php
@@ -1,16 +1,11 @@
 <?php
-
-
-$servername = "localhost"; 
-$username = "root";       
-$password = "";            
-$dbname = "energiahatekony2"; 
-
+$servername = "localhost";
+$username = "energyfelhaszn";
+$password = "energyadmin123--";
+$dbname = "energyfelhaszn";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 
 if ($conn->connect_error) {
     die("Kapcsolódási hiba: " . $conn->connect_error);
 }
-?>
